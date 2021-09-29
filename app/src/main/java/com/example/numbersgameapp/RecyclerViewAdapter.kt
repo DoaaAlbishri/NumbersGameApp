76401bf9f17db1385fc6240fcae5ef82fcc515b0
Doaa <Duaa_ali@outlook.sa>
2021-09-29 20:49:12 +0300

    Second

--- app/src/main/java/com/example/numbersgameapp/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/numbersgameapp/RecyclerViewAdapter.kt
@@ -1,12 +1,15 @@
 package com.example.numbersgameapp
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.item_row.view.*
+import kotlin.random.Random
 
-class RecyclerViewAdapter(private val words: ArrayList<String>) : RecyclerView.Adapter<RecyclerViewAdapter.ItemViewHolder>(){
+class RecyclerViewAdapter(private val guesses: ArrayList<String>,private val lefts: ArrayList<String>) : RecyclerView.Adapter<RecyclerViewAdapter.ItemViewHolder>(){
     class ItemViewHolder(itemView : View) :RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -20,14 +23,21 @@ class RecyclerViewAdapter(private val words: ArrayList<String>) : RecyclerView.A
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val word=words[position]
+        val guess=guesses[position]
+        val left=lefts[position]
 
         holder.itemView.apply{
-
-           tv.text=word
+            var ncolor = listOf<String>("#FFFFFFFF","#FF6200EE","#FFBB86FC","#f1e888")
+            var color = ncolor[Random.nextInt(3)]
+            //val White ="#FFFFFFFF"
+            //val purple_500 ="#FF6200EE"
+            //val purple_200 = "#FFBB86FC"
+            cv.setBackgroundColor(Color.parseColor(color))
+            tv.text=guess
+            tv1.text=left
         }
     }
 
-    override fun getItemCount(): Int =words.size
+    override fun getItemCount(): Int = guesses.size
 
 }
